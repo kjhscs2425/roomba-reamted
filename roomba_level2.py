bab717d40176d41997a921cd6274c37d1659b0f7
reamted <nroussos2028@kehillahstudent.org>
2024-09-11 11:49:21 -0700

    turtule done (exept for room 2)

--- roomba_level2.py
+++ roomba_level2.py
@@ -2,7 +2,7 @@
 # Roomba in Python
 # This file implements an algorithm for a roomba cleaning a room.
 #
-# Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
+# Author: Nick <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
  
 from turtle import right, left, forward, backward, speed
@@ -14,9 +14,29 @@ speed(7)
 # Draw the Level 2 version of the room
 window = room.draw_room(level = 2)
 
+
 ###
 # Start your code here
- 
+
+square=40
+
+height=15
+width=20
+
+forward((height-1)*square)
+for i in range(15):
+    for e in range(2):
+            
+                vertical_arm_lenght = (height - i-e)*square
+                horizontal_arm_lenght = (width - i-e) * square
+    left(90)
+    forward(horizontal_arm_lenght)
+    left(90)
+    forward(vertical_arm_lenght)
+    
+    
+    
+    
  
  
 # End your code here
--- roomba_level5.py
+++ roomba_level5.py
@@ -5,7 +5,7 @@
 # Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
  
-from turtle import right, left, forward, backward
+from turtle import right, left, forward, backward, speed
 import room
 
 # THIS PARAMETER CAN CHANGE!!!
@@ -13,12 +13,136 @@ import room
 # (You are allowed to use this parameter in your code)
 n_alcoves = 3
 
+speed(10) 
 # Draw the Level 5 version of the room
 window = room.draw_room(level = 5, n_alcoves = n_alcoves)
 
 ###
 # Start your code here
  
+forward(40*14)
+left(180)
+forward(40)
+left(90)
+forward(120)
+
+backward(240)
+right(90)
+forward(40)
+right(90)
+forward(40)
+for i in range(3):
+    backward(320)
+    left(90)
+    forward(40)
+    right(90)
+    forward(320)
+    left(90)
+    forward(40)
+    right(90)
+backward(320)
+forward(280)
+left(90)
+forward(40)
+right(90)
+backward(240)
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(120)
+right(90)
+forward(40*5)
+backward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(40)
+backward(40)
+right(90)
+forward(40)
+right(90)
+forward(80)
+right(90)
+forward(40)
+left(90)
+forward(40)
+backward(40)
+right(90)
+forward(40)
+right(90)
+forward(80)
+backward(40)
+left(90)
+
+forward(480)
+
+
+left(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(40)
+backward(40)
+
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(40)
+backward(40)
+
+
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(40)
+backward(80)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+left(90)
+forward(240)
+right(90)
+
+#new stuff alert
+speed(4)
+forward(240)
+right(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(40)
+backward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(40)
+backward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward(40)
+backward(40)
+left(90)
+forward(40)
+
+
+
  
  
 # End your code here
